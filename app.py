ff4a937f5693b460a63fd464bb273494d9fd833f
Poojan Thaker <poojanthaker@gmail.com>
2021-05-30 15:29:17 +0530

    change host

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
     app.logLevel='WARNING'
     import os
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=True,port=port)
+    socketio.run(app, debug=True,host='0.0.0.0', port=port)
     
